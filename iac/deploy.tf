b214c8295f3f33c635f85204e8281320f0382f0d
Jeeva-prof <jeeva.mpcc@gmail.com>
2024-09-22 00:50:55 +0530

    Update deploy.tf

--- iac/deploy.tf
+++ iac/deploy.tf
@@ -1,3 +1,26 @@
+// Create Security Group
+resource "aws_security_group" "app_SG" {
+  name        = "finance_sg"
+  
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "finance_sg"
+  }
+}
 resource "aws_key_pair" "deployer" {
   key_name   = "deployer-key"
   public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP/vYB7ZgCxcwRPGP4kJoPttUs5aCrsBWj0QdgBVdJ8D root@master"
@@ -7,6 +30,7 @@ resource "aws_instance" "EC2" {
   instance_type = "t2.micro"
   key_name        = "deployer-key"
   availability_zone = "ap-south-1a"
+  vpc_security_group_ids = [aws_security_group.app_SG.id]
   associate_public_ip_address = true
   
   tags = {
@@ -20,7 +44,7 @@ resource "aws_instance" "EC21" {
   key_name        = "deployer-key"
   availability_zone = "ap-south-1a"
   associate_public_ip_address = true
-  
+  vpc_security_group_ids = [aws_security_group.app_SG.id]
   tags = {
     Name = "production-server"
   }
