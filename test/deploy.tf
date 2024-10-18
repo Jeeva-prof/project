da10c1ff98fc6bad112d850ae9c7799998491332
Jeeva-prof <jeeva.mpcc@gmail.com>
2024-10-18 18:46:50 +0530

    Update deploy.tf

--- test/deploy.tf
+++ test/deploy.tf
@@ -23,7 +23,7 @@ resource "aws_security_group" "app_SG" {
 }
 resource "aws_key_pair" "deployer" {
   key_name   = "deployer-key"
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP/vYB7ZgCxcwRPGP4kJoPttUs5aCrsBWj0QdgBVdJ8D root@master"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINbgziXYhMEhfAMi5T/jrKajtzSLb0w7JpkiiNeTZAG+ root@master"
 }
 resource "aws_instance" "EC2" {
   ami           = "ami-0b8eb446c5e792d0d" 
